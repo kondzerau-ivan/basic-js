5633afe0ddc476f10df117a3a4766a3bce2f2ad7
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-17 12:41:32 +0100

    feat: dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  var test = '';
+  for (var i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      test += members[i].trim()[0].toUpperCase();
+    }
+  }
+  return test.split('').sort().join('');
 };
