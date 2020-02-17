1e78a0a7f610144d405c5809716a2cd8da5be40c
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-17 08:18:59 +0100

    feat: count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if (matrix.length === 0) {
+    return 0;
+  }
+  var sum = 0;
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+      if (/^\^\^$/.test(matrix[i][j])) {
+        sum++;
+      }
+    }
+  }
+  return sum;
 };
