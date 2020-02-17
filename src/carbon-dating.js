c204b6d26dbbd9aa8ddeb42b39954f44687e930d
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-17 11:48:10 +0100

    feat: base requirements carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sample) {
+  if (sample === false) {
+    return false;
+  }
+  if (typeof(sample) !== 'string') {
+    return false;
+  }
+  if (!(/^[0-9]*\.[0-9]+$/.test(sample))) {
+    return false;
+  }
+  var FINAL_ACTIVITY = Math.ceil(parseFloat(sample));
+  if (FINAL_ACTIVITY < 0 || FINAL_ACTIVITY > 15) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / FINAL_ACTIVITY) * HALF_LIFE_PERIOD / 0.693);
 };
