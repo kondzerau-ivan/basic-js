4b6d639fbc60a395205d34a5fad5b9e0d6a9285e
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-18 02:30:09 +0100

    feat: extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,37 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    var separator = '';
+    var repeatTimes = 1;
+    var addition = '';
+    var result = '';
+    var additionSeparator = '';
+    var additionRepeatTimes = 1;
+    if (options['separator'] === undefined) {
+        separator = '+';
+    } else {
+        separator = options['separator'];
+    }
+    if (!(options['repeatTimes'] === undefined)) {
+        repeatTimes = options['repeatTimes'];
+    }
+    if (!(options['addition'] === undefined)) {
+        addition = options['addition']
+    }
+    if (!(options['additionRepeatTimes'] === undefined)) {
+        additionRepeatTimes = options['additionRepeatTimes'];
+    }
+    if (!(options['additionSeparator'] === undefined)) {
+        additionSeparator = options['additionSeparator'];
+    }
+    for (var i = 0; i < repeatTimes; i++) {
+        result += str;
+        for (var j = 0; j < additionRepeatTimes; j++) {
+            result += addition + additionSeparator;
+        }
+        if (additionSeparator) {
+            result = result.slice(0, -additionSeparator.length);
+        }
+        result += separator;
+    }
+    return result.slice(0, -separator.length);
 };
   
