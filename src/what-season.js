2031bbf3b5ac5d243d11d8cb8828962fe75f6a23
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-18 01:05:57 +0100

    feat: what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,24 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  switch(date.getMonth()) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+      break;
+    default:
+      return 'autumn';
+  }
 };
