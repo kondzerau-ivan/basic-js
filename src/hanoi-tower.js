da99e3a58cb8eb9c663a52c35101c8e2b3c95dab
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-18 10:34:46 +0100

    fix: hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     var shiftSteps = Math.pow(2, disksNumber) - 1;
-    var shiftTime = shiftSteps / turnsSpeed * 3600;
+    var shiftTime = shiftSteps / (turnsSpeed / 3600);
     return {
         turns: shiftSteps,
         seconds: shiftTime
