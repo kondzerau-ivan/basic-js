9d16b80a18af29e202edd3ef3477abcd2d86efc3
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-19 07:15:07 +0100

    feat: transform-array.js (3/4)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,23 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw 'Error';
+    }
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            i++;
+        }
+        if (arr[i] === '--discard-prev' && newArr.length > 0) {
+            newArr.pop();
+        }
+        if (arr[i] === '--double-next' && i < arr.length - 1) {
+            newArr.push(arr[i + 1]);
+        }
+        if (arr[i] === '--double-prev' && i > 1) {
+            newArr.push(arr[i - 1]);
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
