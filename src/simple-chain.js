7a58ececeb24865adc8aa93b7fe9b0916f44f493
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-23 18:09:51 +0100

    feat: simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,32 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push('( ' + value + ' )');
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (Number.isInteger(position) &&
+        position <= this.chain.length &&
+        position > 0) {
+          this.chain.splice(position - 1, 1);
+          return this;
+        } else {
+          this.chain = [];
+          throw new Error();
+        }
+    
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    var res = this.chain.join('~~');
+    this.chain = [];
+    return res;
   }
 };
 
